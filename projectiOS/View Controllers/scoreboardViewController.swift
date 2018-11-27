d736c6a624bddfab0353668bbc922a5e40c52332
Tejas Jadhav <tejas@tejass-MacBook-Pro.local>
2018-11-26 19:26:38 -0500

    first commit

---
+++ projectiOS/View Controllers/scoreboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  scoreboardViewController.swift
+//  projectiOS
+//
+//  Created by Tejas Jadhav on 2018-11-26.
+//  Copyright © 2018 Tejas Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class scoreboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
